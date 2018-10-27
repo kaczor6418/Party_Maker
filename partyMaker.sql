d5a4034dd0b95a46a26cbb550a062011f670acca
kobold97 <kobold97@gmail.com>
2018-10-28 02:42:04 +0300

    add users table description

---
+++ partyMaker.sql
@@ -0,0 +1,15 @@
+CREATE TABLE users(
+	user_id int(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+	login varchar(20) NOT NULL UNIQUE,
+	password varchar(255) NOT NULL,
+	name varchar(20) NOT NULL,
+	surname varchar(20) NOT NULL,
+	birthDate DATE NOT NULL,
+	regDate TIMESTAMP NOT NULL,
+	lastSuccessfulLogin TIMESTAMP NOT NULL,
+	attempts INT UNSIGNED DEFAULT 0,
+	lastUnsuccessfulLogin TIMESTAMP NOT NULL,
+    	lastActive TIMESTAMP NOT NULL,
+	email varchar(255) NOT NULL,
+	PRIMARY KEY (user_id)
+);
